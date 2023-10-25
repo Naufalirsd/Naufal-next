6c20e481a6320b92c9aff0ef127271600676e337
Naufal Irsyad Ihsan <naufalirsd.ppqita@gmail.com>
2023-10-25 11:41:31 +0700

    first commit

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.method)
+  console.log(req.body);
+
+  if (req.method === 'POST') {
+    res.status(200).json({message: 'Hello ' + req.body.displayName});
+  } else {
+    res.status(404).json({error: true, message: 'method tidak diijinkan'})
+  }
 }
